f884533bde93a64ad43a334fb0853a1456fc2a69
damianx1337 <dn.sobc@gmail.com>
2024-05-15 21:27:21 +0200

    updated minikube start script

--- minikube-start-stop.sh
+++ minikube-start-stop.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+export CONTAINERS_MACHINE_PROVIDER=applehv
+
 if [ $1 == "start" ]; then
 	echo "starting podman machine and minikube k8s cluster..."
-	podman machine init --cpus 6 --memory 8192 --disk-size 15 minikube-6-8
+	podman machine init --cpus 6 --memory 8000 --disk-size 15 minikube-6-8
 	podman system connection default minikube-6-8-root
 	podman machine start minikube-6-8
-	minikube start -p 1-node-cluster --nodes 1 --memory 7000 --cpus 4 --kubernetes-version=v1.26.3 --driver=podman --container-runtime=containerd
+	minikube start -p 1-node-cluster --nodes 1 --memory 6000 --cpus 4 --kubernetes-version=v1.28.10 --driver=podman --container-runtime=containerd
 	minikube profile list
 	echo "DONE"
 elif [ $1 == "stop" ]; then
@@ -14,7 +16,7 @@ elif [ $1 == "stop" ]; then
 	minikube delete -p 1-node-cluster
 	podman machine stop minikube-6-8
 	podman machine rm minikube-6-8
-	echo "DONE"
+	echo "CLEANUP DONE"
 fi
 
 exit 0
